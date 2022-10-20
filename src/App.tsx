5565f05de2dbca764d5d837ddb9b933ad33fee81
imtiaj <imtiajrex@gmail.com>
2022-10-21 01:07:35 +0600

    added chrome storage editor

--- src/App.tsx
+++ src/App.tsx
@@ -4,35 +4,36 @@ import { useEffect } from "react";
 import List from "./components/List";
 import { useForm } from "@mantine/form";
 import Form from "./components/Form";
-import { useListState, useLocalStorage } from "@mantine/hooks";
+import { useListState } from "@mantine/hooks";
+import { useChromeStorageSync } from "use-chrome-storage";
 export type dataType = {
 	name: string;
 	id: string;
 };
 function App() {
-	const [data, setData] = useLocalStorage<dataType[]>({
-		defaultValue: [],
-		key: "data",
-	});
-	const [state, handlers] = useListState(data);
-	useEffect(() => {
-		setData(state);
-	}, [state]);
-
+	const [data, setData, isPersistent, error] = useChromeStorageSync("data", []);
+	const [state, handlers] = useListState(data as dataType[]);
 	const form = useForm({
 		initialValues: {
 			text: "",
 		},
 	});
+	if (error) {
+		console.log(error);
+		return <></>;
+	}
 	const handleSubmit = (values: { text: string }) => {
-		handlers.insert(state.length, {
+		const newItem = {
 			name: values.text,
 			id: Math.random().toString(),
-		});
+		};
+		handlers.insert(0, newItem);
+		setData([newItem, ...state]);
 		form.reset();
 	};
 	const handleDelete = (index: number) => {
 		handlers.remove(index);
+		setData(state.filter((item) => item.id !== state[index].id));
 	};
 	const handleCrossOff = (index: number) => {};
 	return (
